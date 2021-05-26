370af8b9ca505f680f129405e892378c6d4ff0e2
lunseq <43300925+lucasns97@users.noreply.github.com>
2021-05-26 12:21:39 -0300

    Update

---
+++ src/js/images.js
@@ -0,0 +1,6 @@
+const images = [
+    { id: 1, src: './../assets/pdf/zine_mac_img/zine_mac_01.jpg', title: 'foo', description: 'bar' },
+    { id: 2, src: './../assets/pdf/zine_mac_img/zine_mac_01.jpg', title: 'foo', description: 'bar' },
+  ];
+  
+  export default images;
