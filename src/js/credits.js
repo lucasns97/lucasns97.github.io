a74ff7881a8d772df6df4526a12ddacd67e9488f
lunseq <43300925+lucasns97@users.noreply.github.com>
2021-08-08 11:50:35 -0300

    Create credits.js

---
+++ src/js/credits.js
@@ -0,0 +1,44 @@
+const TOPICS_URL = "https://raw.githubusercontent.com/lucasns97/lucasns97.github.io/master/src/json/topics.json"
+
+const vm = new Vue({
+    el: '#credits',
+    data: {
+        inputData: '',
+        activeMenu: '3',
+        loading: false,
+        topicsData: [],
+        filteredTopicsData: [],
+        genderPairs: {
+            'ao': ['a', 'o'],
+            'ae': ['a', 'e'],
+            'a_': ['a', '•'],
+        },
+        selectedGender: 0,
+    },
+    async mounted() {
+        this.selectedGender = this.randomGender()
+    },
+    methods: {
+        randomGender() {
+            if (Math.random() < 0.5) return 0
+            else return 1
+        },
+        changeGender() {
+            this.selectedGender = (this.selectedGender + 1) % 2
+        },
+        handleMenu(key, keyPath) {
+            this.activeMenu = key;
+        },
+        ajustText(text, language) {
+            this.introductionCorpus[language] = text.split('\n');
+        },
+        changeLanguage(lang) {
+            this.language = lang;
+        },
+        navigateTo(path) {
+            parent.window.location.href = path
+        },
+    },
+    filters: {},
+    computed: {},
+});
